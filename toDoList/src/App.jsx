cf1076bfd17767c4c16edb7c49f6de7266e4abe1
PauloFonseca <pfonseca.arq@gmail.com>
2023-09-26 11:04:17 -0300

    componente TodoForm e estilizacao dele

--- toDoList/src/App.jsx
+++ toDoList/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Todo from './components/Todo'
+import TodoForm from './components/TodoForm'
 
 function App() {
   const [todos, setTodos] = useState([
@@ -33,6 +34,7 @@ function App() {
           <Todo todo={todo} />
         ))}
       </div>
+      <TodoForm />
     </div>
   )
 }
