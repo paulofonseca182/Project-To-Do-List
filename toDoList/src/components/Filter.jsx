75ad077d95800f0172a1a939164c2e9ddaabe52c
PauloFonseca <pfonseca.arq@gmail.com>
2023-09-26 21:24:15 -0300

    Atualizando o Readme e finalizando o projeto

--- toDoList/src/components/Filter.jsx
+++ toDoList/src/components/Filter.jsx
@@ -1,24 +1,24 @@
 const Filter = ({ filter, setFilter, setSort }) => {
-  return (
-    <div className="filter">
-        <h2>Filtrar:</h2>
-        <div className="filter-options">
-            <div>
-                <p>Status:</p>
-                <select value={filter} onChange={(e) => setFilter(e.target.value)}>
-                    <option value="All">Todas</option>
-                    <option value="Completed">Concluidas</option>
-                    <option value="Incompleted">Pendentes</option>
-                </select>
-            </div>
-            <div>
-                <p>Ordenar:</p>
-                <button onClick={() => setSort('A-Z')}>A-Z</button>
-                <button onClick={() => setSort('Z-A')}>Z-A</button>
+    return (
+        <div className="filter">
+            <h2>Filtrar:</h2>
+            <div className="filter-options">
+                <div>
+                    <p>Status:</p>
+                    <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+                        <option value="All">Todas</option>
+                        <option value="Completed">Concluidas</option>
+                        <option value="Incompleted">Pendentes</option>
+                    </select>
+                </div>
+                <div>
+                    <p>Ordenar:</p>
+                    <button onClick={() => setSort('A-Z')}>A-Z</button>
+                    <button onClick={() => setSort('Z-A')}>Z-A</button>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Filter
--- toDoList/src/components/Todo.jsx
+++ toDoList/src/components/Todo.jsx
@@ -1,18 +1,18 @@
 const Todo = ({ todo, removeTodo, completeTodo }) => {
-  return (
-    <div>
-      <div className='todo' style={{textDecoration: todo.isCompleted ? "line-through" : "" }}>
-            <div className='content'>
-              <p>{todo.text}</p>
-              <p>({todo.category})</p>
+    return (
+        <div>
+            <div className='todo' style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}>
+                <div className='content'>
+                    <p>{todo.text}</p>
+                    <p>({todo.category})</p>
+                </div>
+                <div>
+                    <button className='complete' onClick={() => completeTodo(todo.id)}>Concluir</button>
+                    <button className='remove' onClick={() => removeTodo(todo.id)}>Excluir</button>
+                </div>
             </div>
-            <div>
-              <button className='complete' onClick={() => completeTodo(todo.id)}>Concluir</button>
-              <button className='remove' onClick={() => removeTodo(todo.id)}>Excluir</button>
-            </div>
-          </div>
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Todo
